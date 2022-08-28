3195768408001866167e0e98a8f4e415f4cd6412
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-28 09:47:15 -0700

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,8 @@
+class ManchesterUnited:
+
+    def current2022Season:
+        DaviddeGrea = "N/A" # goalkeeper
+        TomHeaton = "N/A" # goalkeeper
+        VictorLindelof = [0,0,1,1,1,0]
+        avggoalVictor = victor.sum
+        
