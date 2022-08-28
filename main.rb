fa58f56f346d403e2765bb231a9492715b43ed74
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-28 13:47:22 -0700

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,8 +1,104 @@
+# Marvish Chandra
 class ManchesterUnited:
 
-    def current2022Season:
+    def current2022Season():
         DaviddeGrea = "N/A" # goalkeeper
         TomHeaton = "N/A" # goalkeeper
         VictorLindelof = [0,0,1,1,1,0]
-        avggoalVictor = victor.sum
-        
+        avggoalVictor = VictorLindelof.sum
+        PhilJones = [0,0,0,0,0,0,0,0,0,1,0,1,0,0]
+        avggoalJones = PhilJones.sum
+        HarryMaguire = [0,1,2,1,0,3,2,2,0]
+        avggoalHarry = HarryMaguire.sum
+        RaphaelVarane = [0,1]
+        avggoalRaphael = RaphaelVarane.sum
+        DiogoDalot = [0,0,0,0,0]
+        avggoalDiogo = DiogoDalot.sum
+        LukeShaw = [0,0,1,0,1,0,0,0,0,0,0]
+        avgoalLuke = LukeShaw.sum
+        Aaron_Wan_Bissaka = [0,0,2,0,0,0,0,0]
+        avggoalAaron = Aaron_Wan_Bissaka.sum
+        BrandonWilliams = [0,0,0,0,0,0,0,1]
+        avggoalBrandon = BrandonWilliams.sum
+        AxelTuanzebe = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+        avggoalAxel = AxelTuanzebe.sum
+        TyrellMalacia = [0]
+        avggoalTyrell = TyrellMalacia.sum
+        LisandroMartinez = [0]
+        avggoalLisandro = LisandroMartinez.sum
+        TedenMengi = [0,0,0,0,0,0,0,0]
+        avggoalTeden = TedenMengi.sum
+        Fred = [0,4,1,0,1]
+        avggoalFred = Fred.sum
+        BrunoFernandes = [1,10,18,8]
+        avggoalBruno = BrunoFernandes.sum
+        ScottMcTominay = [0,1,4,4,2,0,0]
+        avggoalScott = ScottMcTominay.sum
+        HannibalMejbri = [0,0,0]
+        avggoalHannibal = HannibalMejbri.sum
+        AlejandroGarnacho = [0,0]
+        avggoalAlejandro = AlejandroGarnacho.sum
+        FacundoPellistri = [0,0,0]
+        avggoalFacundo = FacundoPellistri.sum
+        Donny_van_de_Beek = [0,0,0,1,3,1,1,1]
+        avggoalDonny = Donny_van_de_Beek.sum
+        JamesGarner = [0,0,0,0,0,0,0,0]
+        avggoalJames = JamesGarner.sum
+        TahithChong = [0,0,0,0,0]
+        avggoalTahith = TahithChong.sum
+        ChristianEriksen = [0,0,2,2,8,10,8,6,10,7]
+        avggoalChristian = ChristianEriksen.sum
+        Casemiro = [0]
+        avggoalCasemiro = Casemiro.sum
+        CristianoRonaldo = [0,18,18,31,17,9,5,4]
+        avggoalCristiano = CristianoRonaldo.sum
+        MarcusRashford = [1,4,11,17,10,7,5,5]
+        avggoalMarcus = MarcusRashford.sum
+        JadonSancho = [1,3,0]
+        avggoalJadon = JadonSancho.sum
+        AnthonyElanga = [0,2,1,1]
+        avggoalAnthony = AnthonyElanga.sum
+        AnthonyMartial = [0,0,1,1,4,17,10,9,4,11]
+        avggoalAnthonyM = AnthonyMartial.sum
+        AmadDiallo = [0,0,0,0,0,0]
+        avggoalAmad = AmadDiallo.sum
+        SholaShoretire = [0,0,0,0,0]
+        avggoalShola = SholaShoretire.sum
+    end
+    def main():
+        print(DaviddeGrea)
+        print(TomHeaton)
+        print(avggoalVictor)
+        print(avggoalJones)
+        print(avggoalHarry)
+        print(avggoalRaphael)
+        print(avggoalDiogo)
+        print(avgoalLuke)
+        print(avggoalAaron)
+        print(avggoalBrandon)
+        print(avggoalAxel)
+        print(avggoalTyrell)
+        print(avggoalLisandro)
+        print(avggoalTeden)
+        print(avggoalFred)
+        print(avggoalBruno)
+        print(avggoalScott)
+        print(avggoalHannibal)
+        print(avggoalAlejandro)
+        print(avggoalFacundo)
+        print(avggoalDonny)
+        print(avggoalJames)
+        print(avggoalTahith)
+        print(avggoalChristian)
+        print(avggoalCasemiro)
+        print(avggoalCristiano)
+        print(avggoalMarcus)
+        print(avggoalJadon)
+        print(avggoalAnthony)
+        print(avggoalAnthonyM)
+        print(avggoalAmad)
+        print(avggoalShola)
+    end 
+
+
+
